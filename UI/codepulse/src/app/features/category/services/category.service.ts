6f194830dce103bd3bb101749f00cb56b5efbf2f
Kevin Klein <kevink@e-claim.com>
2024-07-11 13:15:13 -0500

    51 create update category service method and connect to edit category component

--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
 import { environment } from 'src/environments/environment';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +25,9 @@ export class CategoryService {
 
   getCategoryById(id: string): Observable<Category> {
     return this.http.get<Category> (`${environment.apiBaseUrl}/api/categories/${id}`)
+  }
+
+  updateCategory(id: string, UpdateCategoryRequest: UpdateCategoryRequest) : Observable<Category> {
+    return this.http.put<Category>(`${environment.apiBaseUrl}/api/categories/${id}`, UpdateCategoryRequest);
   }
 }
