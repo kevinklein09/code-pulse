065e513b660098baedff827a17bff02b8f480672
Kevin Klein <kevink@e-claim.com>
2024-07-16 13:25:41 -0500

    change getallblogposts action method - return categories

--- API/CodePulse.API/CodePulse.API/Controllers/BlogPostsController.cs
+++ API/CodePulse.API/CodePulse.API/Controllers/BlogPostsController.cs
@@ -93,6 +93,12 @@ namespace CodePulse.API.Controllers
                     ShortDescription = blogPost.ShortDescription,
                     Title = blogPost.Title,
                     UrlHandle = blogPost.UrlHandle,
+                    Categories = blogPost.Categories.Select(x => new CategoryDto
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        UrlHandle = x.UrlHandle
+                    }).ToList()
                 });
             }
             return Ok(response);
