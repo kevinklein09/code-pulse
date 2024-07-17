05ff14d36f8c14d80869e58af519826e491421a8
Kevin Klein <kevink@e-claim.com>
2024-07-17 12:53:55 -0500

    submit form to update blogpost and categories

--- UI/codepulse/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { BlogPostService } from '../services/blog-post.service';
 import { BlogPost } from '../models/blog-post.model';
 import { CategoryService } from '../../category/services/category.service';
 import { Category } from '../../category/models/category.model';
+import { UpdateBlogPost } from '../models/update-blog-post.model';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -19,24 +20,27 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
 
 
   routeSubscription?: Subscription;
+  updateBlogPostSubscription?: Subscription;
+  getBlogPostDescription?: Subscription;
 
 
   constructor(private route: ActivatedRoute,
     private blogPostService: BlogPostService,
-    private categoryService: CategoryService) {
+    private categoryService: CategoryService,
+    private router: Router) {
 
   }
 
   ngOnInit(): void {
     this.categories$ = this.categoryService.getAllCategories();
-    
+
     this.routeSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
 
         // Get BlogPost From API
         if(this.id) {
-          this.blogPostService.getBlogPostById(this.id).subscribe({
+          this.getBlogPostDescription = this.blogPostService.getBlogPostById(this.id).subscribe({
             next: (response) => {
               this.model = response;
               this.selectedCategories = response.categories.map(x => x.id);
@@ -49,11 +53,32 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
 
 
   onFormSubmit(): void {
+    // convert this model to Request Object
+    if (this.model && this.id) {
+      var updateBlogPost: UpdateBlogPost = {
+        author: this.model.author,
+        content: this.model.content,
+        shortDescription: this.model.shortDescription,
+        featuredImageUrl: this.model.featuredImageUrl,
+        isVisible: this.model.isVisible,
+        publishedDate: this.model.publishedDate,
+        title: this.model.title,
+        urlHandle: this.model.urlHandle,
+        categories: this.selectedCategories ?? []
+      };
 
+      this.updateBlogPostSubscription = this.blogPostService.updateBlogPost(this.id, updateBlogPost)
+      .subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/blogposts');
+        }
+      })
+    }
   }
 
   ngOnDestroy(): void {
-    this.routeSubscription?.unsubscribe()
+    this.routeSubscription?.unsubscribe();
+    this.updateBlogPostSubscription?.unsubscribe();
   }
 
 }
