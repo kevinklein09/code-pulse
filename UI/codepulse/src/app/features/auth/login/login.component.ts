176fa216a8a63b7be62bfac209ef37b6a5e4586f
Kevin Klein <kevink@e-claim.com>
2024-08-02 17:02:07 -0500

    auth service and navbar changes

--- UI/codepulse/src/app/features/auth/login/login.component.ts
+++ UI/codepulse/src/app/features/auth/login/login.component.ts
@@ -30,6 +30,12 @@ export class LoginComponent {
         this.cookieService.set('Authorization', `Bearer ${response.token}`, 
           undefined, '/', undefined, true, 'Strict');
 
+          // Set User
+          this.authService.setUser({
+            email: response.email,
+            roles: response.roles
+          })
+
           // Redirect back to Homepage
           this.router.navigateByUrl('/');
       }
