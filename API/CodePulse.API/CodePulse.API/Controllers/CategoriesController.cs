248cd2b585bbc58737c8aa056343d023fb4a5672
Kevin Klein <kevink@e-claim.com>
2024-08-07 14:07:06 -0500

    add filtering to web API

--- API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -20,9 +20,8 @@ namespace CodePulse.API.Controllers
             this.categoryRepository = categoryRepository;
         }
 
-        // 
         [HttpPost]
-        [Authorize(Roles = "Writer")]
+        //[Authorize(Roles = "Writer")]
         public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryRequestDto request)
         {
             // Map DTO to Domain Model
@@ -45,11 +44,11 @@ namespace CodePulse.API.Controllers
 
             return Ok(response);
         }
-        // GET: https://localhost:7168/api/Categories
+        // GET: https://localhost:7168/api/Categories?query=html
         [HttpGet]
-        public async Task<IActionResult> GetAllCategories()
+        public async Task<IActionResult> GetAllCategories([FromQuery] string? query)
         {
-            var categories = await categoryRepository.GetAllAsync();
+            var categories = await categoryRepository.GetAllAsync(query);
 
             // Map Domain model to DTO
 
